2a8ff05ea3ac12dab53535929591c46287f46541
sean <shonum@gmail.com>
2025-09-02 19:54:49 +0800

    fix: comment out db env vars

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -1,6 +1,6 @@
 // import { keys as analytics } from '@repo/analytics/keys';
 import { keys as auth } from '@repo/auth/keys';
-import { keys as database } from '@repo/database/keys';
+// import { keys as database } from '@repo/database/keys';
 import { keys as email } from '@repo/email/keys';
 import { keys as core } from '@repo/next-config/keys';
 import { keys as observability } from '@repo/observability/keys';
@@ -12,7 +12,7 @@ export const env = createEnv({
     auth(),
     // analytics(),
     core(),
-    database(),
+    // database(),
     email(),
     observability(),
     payments(),
